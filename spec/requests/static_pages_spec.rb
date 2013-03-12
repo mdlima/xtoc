6922115d0fdcdb025014674e368f06f17d93933b
Marcio Lima <mdlima.github@mailnull.com>
2013-03-11 23:58:59 -0300

    Layout and routes.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,45 +2,33 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1',    text: heading) }
+    it { should have_selector('title', text: "xtoc | #{page_title}") }
+  end
+
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the h1 'Home'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Home')
-    end
-
-    it "should have the title 'xtoc'" do
-      visit '/static_pages/home'
-      page.should have_selector('title',
-                        :text => "xtoc")
-    end
+    it { should have_selector('h1',    text: "Home") }
+    it { should have_selector('title', text: "xtoc") }
   end
 
   describe "Help page" do
+    before { visit help_path }
+    let(:heading)    { 'Help' }
+    let(:page_title) { 'Help' }
 
-    it "should have the h1 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
-    end
-
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('title',
-                    :text => "xtoc | Help")
-    end
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
+    before { visit about_path }
+    let(:heading)    { 'About' }
+    let(:page_title) { 'About' }
 
-    it "should have the h1 'About'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About')
-    end
-
-    it "should have the title 'About'" do
-      visit '/static_pages/about'
-      page.should have_selector('title',
-                    :text => "xtoc | About")
-    end
+    it_should_behave_like "all static pages"
   end
 end
